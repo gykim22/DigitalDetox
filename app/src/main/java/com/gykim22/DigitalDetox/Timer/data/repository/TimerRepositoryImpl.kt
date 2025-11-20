612eb14cf7978537a58767f99482c3128445ac33
cloud-23k <k_bright_st@pusan.ac.kr>
2025-11-20 11:50:13 +0900

    Feat(TimerRepositoryImpl.kt): 서브 타이머 로직 구현을 위한 구현체 수정
    - Singleton 어노테이션 제거
    - delta 방식으로 변경

--- app/src/main/java/com/gykim22/DigitalDetox/Timer/data/repository/TimerRepositoryImpl.kt
+++ app/src/main/java/com/gykim22/DigitalDetox/Timer/data/repository/TimerRepositoryImpl.kt
@@ -16,74 +16,47 @@ import javax.inject.Singleton
  * TimerRepository 구현체입니다.
  * @author Kim Giyun
  */
-@Singleton
 class TimerRepositoryImpl @Inject constructor() : TimerRepository {
-    /**
-     * 타이머 현재 경과 시간
-     * 변수 명 뒤에 Millis를 붙여 이 변수가 어떤 단위인지 표기.
-     **/
-    private val _timeElapsedMillis = MutableStateFlow(0L)
+
+    private val _flow = MutableStateFlow(0L)
+    override fun getTimerMillsUpdate(): Flow<Long> = _flow.asStateFlow()
 
     private var timerJob: Job? = null
+    private var lastTimestamp = 0L
+    private var accumulated = 0L
 
-    /**
-     * 타이머의 최신 재개 시간
-     **/
-    private var startTimeMillis: Long = 0L
+    private val scope = CoroutineScope(Dispatchers.Default)
 
-    /**
-     * 일시 정지에 따른 타이머 누적 시간
-     **/
-    private var accumulatedTimeMillis: Long = 0L
-
-    private val timerScope = CoroutineScope(Dispatchers.Default)
-
-    override fun getTimerMillsUpdate(): Flow<Long> {
-        return _timeElapsedMillis.asStateFlow()
-    }
-
-    /**
-     * 타이머 시작
-     */
     override fun startTimer() {
-        if (timerJob?.isActive == true) return
-        startTimeMillis = System.currentTimeMillis()
+        if (timerJob != null) return
 
-        timerJob = timerScope.launch {
+        lastTimestamp = System.currentTimeMillis()
+
+        timerJob = scope.launch {
             while (true) {
-                /* Compose에서 delay가 없다면 UI는 event를 잔뜩 받고 성능이 저하됨. 따라서, 적절한 딜레이로 정확도와 성능 개선 */
                 delay(100)
 
-                val currentTimeMillis = System.currentTimeMillis()
-                val elapsedTimeMillis = currentTimeMillis - startTimeMillis + accumulatedTimeMillis
+                val now = System.currentTimeMillis()
+                val delta = now - lastTimestamp
+                lastTimestamp = now
 
-                _timeElapsedMillis.value = elapsedTimeMillis
+                accumulated += delta
+                _flow.value = accumulated
             }
         }
     }
 
-    /**
-     * 타이머 일시 정지
-     */
     override fun pauseTimer() {
         timerJob?.cancel()
         timerJob = null
-
-        /* 경과 시간을 누적 시간에 저장 */
-        accumulatedTimeMillis = _timeElapsedMillis.value
     }
 
-    /**
-     * 타이머 종료
-     */
     override fun stopTimer() {
         timerJob?.cancel()
         timerJob = null
 
-        /* 전부 초기화 */
-        startTimeMillis = 0L
-        accumulatedTimeMillis = 0L
-        _timeElapsedMillis.value = 0L
+        accumulated = 0L
+        lastTimestamp = 0L
+        _flow.value = 0L
     }
-
 }
