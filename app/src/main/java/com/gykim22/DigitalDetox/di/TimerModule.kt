f8204c2a51031ca5864d0979c8971d2077f563a5
cloud-23k <k_bright_st@pusan.ac.kr>
2025-11-20 11:49:40 +0900

    Feat(TimerModule.kt): 서브 타이머 추가로 인한 로직 수정

--- app/src/main/java/com/gykim22/DigitalDetox/di/TimerModule.kt
+++ app/src/main/java/com/gykim22/DigitalDetox/di/TimerModule.kt
@@ -2,19 +2,46 @@ package com.gykim22.DigitalDetox.di
 
 import com.gykim22.DigitalDetox.Timer.data.repository.TimerRepositoryImpl
 import com.gykim22.DigitalDetox.Timer.domain.repository.TimerRepository
-import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import jakarta.inject.Singleton
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Qualifier
 
+/**
+ * Timer Module입니다.
+ * ViewModelComponent에 의존성을 주입합니다.
+ * @author Kim Giyun
+ */
 @Module
-@InstallIn(SingletonComponent::class)
-abstract class TimerModule {
-    /* 인터페이스(TimerRepository)에 구현체(TimerRepositoryImpl)를 바인딩 */
-    @Binds
-    @Singleton
-    abstract fun bindTimerRepository(
-        timerRepositoryImpl: TimerRepositoryImpl
-    ): TimerRepository
+@InstallIn(ViewModelComponent::class)
+object TimerModule {
+    @PrimaryTimer
+    @Provides
+    @ViewModelScoped
+    fun providePrimaryTimerRepository(): TimerRepository =
+        TimerRepositoryImpl()
+
+    @SubTimer
+    @Provides
+    @ViewModelScoped
+    fun provideSubTimerRepository(): TimerRepository =
+        TimerRepositoryImpl()
 }
+
+/**
+ * 동일 구현체에 Qualifier 어노테이션을 활용해 name tag를 달아 식별합니다.
+ * @author Kim Giyun
+ */
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PrimaryTimer
+
+/**
+ * 동일 구현체에 Qualifier 어노테이션을 활용해 name tag를 달아 식별합니다.
+ * @author Kim Giyun
+ */
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class SubTimer
