6ae64ef2c69213e1884e5c45033d080c21fa512e
cloud-23k <k_bright_st@pusan.ac.kr>
2025-11-20 22:59:09 +0900

    Feat(Note.kt): 노트 기본 구조 설정
    - 노트 제목, 내용, 분류, 생성일자, 타이머 연동(전체 시간, 공부 시간, 휴식 시간), id 값

---
+++ app/src/main/java/com/gykim22/DigitalDetox/Note/domain/model/Note.kt
@@ -0,0 +1,20 @@
+package com.gykim22.DigitalDetox.Note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note(
+    val title: String,
+    val contents: String,
+    val category: String,
+    val timestamp: Long,
+    val total_time: Long,
+    val study_time: Long,
+    val break_time: Long,
+    @PrimaryKey val id: Int? = null
+) {
+    companion object {
+        val categories = listOf("공부", "기타")
+    }
+}
