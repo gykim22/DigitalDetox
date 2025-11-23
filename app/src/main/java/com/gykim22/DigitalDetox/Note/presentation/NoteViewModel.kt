21187c2e6133fef78af2310fc55ef229ede85370
gykim_kr <k_bright_st@pusan.ac.kr>
2025-11-23 20:31:24 +0900

    feat: NoteViewModel에 노트 작성 로직 추가

--- app/src/main/java/com/gykim22/DigitalDetox/Note/presentation/NoteViewModel.kt
+++ app/src/main/java/com/gykim22/DigitalDetox/Note/presentation/NoteViewModel.kt
@@ -2,15 +2,19 @@ package com.gykim22.DigitalDetox.Note.presentation
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.gykim22.DigitalDetox.Note.domain.model.Note
 import com.gykim22.DigitalDetox.Note.domain.use_cases.NoteUseCases
+import com.gykim22.DigitalDetox.Note.domain.util.AddEditNoteEvent
 import com.gykim22.DigitalDetox.Note.domain.util.NoteEvent
 import com.gykim22.DigitalDetox.Note.domain.util.NoteOrder
 import com.gykim22.DigitalDetox.Note.domain.util.OrderType
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -21,18 +25,67 @@ data class NoteState(
     val noteOrder: NoteOrder = NoteOrder.Timestamp(OrderType.Descending),
 )
 
+data class NoteTextFieldState(
+    val text: String = "",
+    val hint: String = "",
+    val isHintVisible: Boolean = true
+)
+
 
 @HiltViewModel
 class NoteViewModel @Inject constructor(
-    private val noteUseCases: NoteUseCases
+    private val noteUseCases: NoteUseCases,
+    savedStateHandle: SavedStateHandle
 ) : ViewModel() {
     private var _noteState = mutableStateOf(NoteState())
     val noteState: State<NoteState> = _noteState
+
+    private var _noteTitle = mutableStateOf(
+        NoteTextFieldState(
+            hint = "제목을 입력해주세요."
+        )
+    )
+    val noteTitle: State<NoteTextFieldState> = _noteTitle
+
+    private var _noteContent = mutableStateOf(
+        NoteTextFieldState(
+            hint = "내용을 입력해주세요."
+        )
+    )
+    val noteContent: State<NoteTextFieldState> = _noteContent
+
+    private var _addEditEventFlow = MutableSharedFlow<UiEvent>()
+    val addEditEventFlow = _addEditEventFlow.asSharedFlow()
+
+    var currentNoteId: Int? = null
+
+
+    sealed class UiEvent {
+        data class ShowSnackbar(val message: String) : UiEvent()
+        object SaveNote : UiEvent()
+    }
 
     private var job: Job? = null
 
     init {
         onNoteEvent(NoteEvent.Order(NoteOrder.Timestamp(OrderType.Descending)))
+        savedStateHandle.get<Int>("noteId")?.let { noteId ->
+            if (noteId != -1) {
+                viewModelScope.launch {
+                    noteUseCases.getNote(noteId)?.also { note ->
+                        currentNoteId = note.id
+                        _noteTitle.value = noteTitle.value.copy(
+                            text = note.title,
+                            isHintVisible = false
+                        )
+                        _noteContent.value = noteContent.value.copy(
+                            text = note.contents,
+                            isHintVisible = false
+                        )
+                    }
+                }
+            }
+        }
     }
 
     fun onNoteEvent(event: NoteEvent) {
@@ -60,6 +113,64 @@ class NoteViewModel @Inject constructor(
                     noteUseCases.deleteNote(event.note)
                 }
             }
+        }
+    }
+
+    fun onAddEditNoteEvent(event: AddEditNoteEvent) {
+        when (event) {
+            is AddEditNoteEvent.EnteredTitle -> {
+                _noteTitle.value = noteTitle.value.copy(
+                    text = event.value
+                )
+            }
+
+            is AddEditNoteEvent.ChangeTitleFocus -> {
+                _noteTitle.value = noteTitle.value.copy(
+                    isHintVisible = !event.focusState.isFocused &&
+                            noteTitle.value.text.isBlank()
+                )
+            }
+
+            is AddEditNoteEvent.EnteredContent -> {
+                _noteContent.value = noteContent.value.copy(
+                    text = event.value
+                )
+            }
+
+            is AddEditNoteEvent.ChangeContentFocus -> {
+                _noteContent.value = noteContent.value.copy(
+                    isHintVisible = !event.focusState.isFocused &&
+                            noteContent.value.text.isBlank()
+                )
+            }
+
+            is AddEditNoteEvent.SaveNote -> {
+                viewModelScope.launch {
+                    try {
+                        noteUseCases.addNote(
+                            Note(
+                                title = noteTitle.value.text,
+                                contents = noteContent.value.text,
+                                category = "공부",
+                                timestamp = System.currentTimeMillis(),
+                                total_time = 0,
+                                study_time = 0,
+                                break_time = 0,
+                                id = currentNoteId
+                            )
+                        )
+                        _addEditEventFlow.emit(UiEvent.SaveNote)
+                    } catch (e: Exception) {
+                        _addEditEventFlow.emit(
+                            UiEvent.ShowSnackbar(
+                                message = e.message ?: "저장에 실패했습니다."
+                            )
+                        )
+                        return@launch
+                    }
+                }
+            }
+
         }
     }
 }
