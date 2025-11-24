4f1019e86a3534f0796fbf5ffc8859cb7b1fe9a0
gykim_kr <k_bright_st@pusan.ac.kr>
2025-11-24 14:23:18 +0900

    chore: 노트 엔티디 주석 작성

--- app/src/main/java/com/gykim22/DigitalDetox/Note/domain/model/Note.kt
+++ app/src/main/java/com/gykim22/DigitalDetox/Note/domain/model/Note.kt
@@ -3,6 +3,18 @@ package com.gykim22.DigitalDetox.Note.domain.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/**
+ * 노트 엔티티입니다.
+ * @property title 노트의 제목입니다.
+ * @property contents 노트의 내용입니다.
+ * @property category 노트의 카테고리입니다.
+ * @property timestamp 노트가 생성된 시간입니다.
+ * @property total_time 전체 시간입니다.
+ * @property study_time 공부 시간입니다.
+ * @property break_time 휴식 시간입니다.
+ * @property id 노트의 Primary Key id입니다. 자동 생성됩니다.
+ * @author Kim Giyun
+ */
 @Entity
 data class Note(
     val title: String,
