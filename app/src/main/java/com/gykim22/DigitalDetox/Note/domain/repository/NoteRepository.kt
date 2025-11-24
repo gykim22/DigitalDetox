73178837a0d6f48ae3bfdbed823b1d937ee0d6a5
gykim_kr <k_bright_st@pusan.ac.kr>
2025-11-24 14:24:40 +0900

    chore: 노트 레포지토리 주석작성

--- app/src/main/java/com/gykim22/DigitalDetox/Note/domain/repository/NoteRepository.kt
+++ app/src/main/java/com/gykim22/DigitalDetox/Note/domain/repository/NoteRepository.kt
@@ -3,6 +3,10 @@ package com.gykim22.DigitalDetox.Note.domain.repository
 import com.gykim22.DigitalDetox.Note.domain.model.Note
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * 노트 레포지토리 인터페이스입니다.
+ * @author Kim Giyun
+ */
 interface NoteRepository {
 
     fun getAllNotes(): Flow<List<Note>>
