d6238a4ddb0b975e7fcf0ac1070acdbf434ca274
gykim_kr <k_bright_st@pusan.ac.kr>
2025-11-24 14:24:14 +0900

    chore: 노트 정렬 기준 클래스 주석 작성

--- app/src/main/java/com/gykim22/DigitalDetox/Note/domain/util/NoteOrder.kt
+++ app/src/main/java/com/gykim22/DigitalDetox/Note/domain/util/NoteOrder.kt
@@ -1,5 +1,10 @@
 package com.gykim22.DigitalDetox.Note.domain.util
 
+/**
+ * 노트의 정렬 기준을 정의합니다.
+ * 기준은 제목, 생성 시각, 카테고리, 공부 시간, 휴식 시간, 전체 시간입니다.
+ * @author Kim Giyun
+ */
 sealed class NoteOrder(val orderType: OrderType) {
     class Title(orderType: OrderType) : NoteOrder(orderType)
     class Timestamp(orderType: OrderType) : NoteOrder(orderType)
