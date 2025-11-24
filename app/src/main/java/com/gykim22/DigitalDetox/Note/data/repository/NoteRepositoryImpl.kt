ddb8fb8245dc57f44ca41f0f84a36993ae46bb29
gykim_kr <k_bright_st@pusan.ac.kr>
2025-11-24 14:24:53 +0900

    chore: 노트 Dao 주석 작성

--- app/src/main/java/com/gykim22/DigitalDetox/Note/data/repository/NoteRepositoryImpl.kt
+++ app/src/main/java/com/gykim22/DigitalDetox/Note/data/repository/NoteRepositoryImpl.kt
@@ -4,6 +4,11 @@ import com.gykim22.DigitalDetox.Note.data.data_source.NoteDao
 import com.gykim22.DigitalDetox.Note.domain.model.Note
 import com.gykim22.DigitalDetox.Note.domain.repository.NoteRepository
 
+/**
+ * 노트 레포지토리 구현체입니다.
+ * @property dao 노트 데이터 액세스 객체입니다.
+ * @author Kim Giyun
+ */
 class NoteRepositoryImpl(
     private val dao: NoteDao
 ) : NoteRepository {
